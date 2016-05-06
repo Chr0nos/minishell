b73417d7234f41c0a96dc1a5ae61d04488d971be
Sebastien N <snicolet@student.42.fr>
2016-05-06 22:55:47 +0200

    ajout de la gestion des erreurs de cd

--- cd.c
+++ cd.c
@@ -6,13 +6,15 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 18:59:20 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/06 20:36:14 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/06 22:54:57 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <unistd.h>
+#include <sys/stat.h>
 
-int		minishell_cd_home(t_list *env)
+int			minishell_cd_home(t_list *env)
 {
 	char	*home;
 
@@ -20,15 +22,43 @@ int		minishell_cd_home(t_list *env)
 	if (!home)
 		ft_putendl_fd("minishell: cd: error: no HOME environement variable", 2);
 	else
-	{
-		ft_printf("minishell: cd: %s\n", home);
-	}
+		chdir(home);
 	return (-1);
 }
 
-int		minishell_cd(int ac, char **av, t_list **env, const char *cmd)
+static int	minishell_cd_error(const char *str, int ret)
 {
-	ft_putendl("cd requested");
+	ft_putstr_fd("minishell: error: cd: ", 2);
+	ft_putendl_fd(str, 2);
+	return (ret);
+}
+
+/*
+** returns:
+** 0 in case of success
+** -1 : not a dir
+** -2 : permission denied
+** -3 : no such file or directory
+*/
+
+static int	minishell_cd_trydir(const char *dir)
+{
+	struct stat		st;
+
+	if (lstat(dir, &st) == 0)
+	{
+		if (!(st.st_mode & S_IFDIR))
+			return (minishell_cd_error("not a directory", -1));
+		else if (!(st.st_mode & R_OK))
+			return (minishell_cd_error("permission denied", -2));
+		return (0);
+	}
+	else
+		return (minishell_cd_error("no such file or directory", -3));
+}
+
+int			minishell_cd(int ac, char **av, t_list **env, const char *cmd)
+{
 	if (ac == 1)
 		return (minishell_cd_home(*env));
 	else if (ac > 2)
@@ -36,8 +66,11 @@ int		minishell_cd(int ac, char **av, t_list **env, const char *cmd)
 		ft_putendl_fd("minishell: error: cd: too many parameters", 2);
 		return (-1);
 	}
-	if (ft_strcmp(av[1], "."))
+	if (!ft_strcmp(av[1], "."))
 		return (-1);
+	if ((!minishell_cd_trydir(av[1])) && (chdir(av[1]) != 0))
+		ft_putendl_fd("minishell: error: cd: failed to change directory", 2);
+	//else if (!ft_strcmp(av[1], ".."))
 	(void)cmd;
 	(void)env;
 	(void)av;
