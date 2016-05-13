d39a11dcc630c89571537c8800085f9d797287a1
Sebastien N <snicolet@student.42.fr>
2016-05-13 13:48:17 +0200

    fix de la detection des fichiers/dossiers, fix de cd ~

--- cd.c
+++ cd.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/05 18:59:20 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/11 21:12:54 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/13 13:47:38 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,10 +50,8 @@ static int	minishell_cd_trydir(const char *dir)
 
 	if (lstat(dir, &st) == 0)
 	{
-		if (!(st.st_mode & (S_IFDIR | S_IFLNK)))
+		if ((!S_ISDIR(st.st_mode)) && (!S_ISLNK(st.st_mode)))
 			return (minishell_cd_error("not a directory", -1));
-		//else if (st.st_mode & S_IFREG)
-		//	return (minishell_cd_error("not a directory", -3));
 		else if (access(dir, X_OK) < 0)
 			return (minishell_cd_error("permission denied", -2));
 		return (0);
@@ -88,14 +86,11 @@ int			minishell_cd(int ac, char **av, t_list **env)
 	t_env	*e;
 
 	if (ac == 1)
-		return (minishell_cd_home(env));
+		minishell_cd_home(env);
 	else if (ac > 2)
-	{
 		ft_putendl_fd("minishell: error: cd: too many parameters", 2);
-		return (-1);
-	}
 	else if (!ft_strcmp(av[1], "~"))
-		return (minishell_cd_home(env));
+		minishell_cd_home(env);
 	else if (!ft_strcmp(av[1], "-"))
 	{
 		e = minishell_getenv_byname(*env, "OLDPWD");
