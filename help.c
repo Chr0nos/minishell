f4be23cad177aa58dc69b1c025a6a01cfa267a24
Sebastien N <snicolet@student.42.fr>
2016-05-19 01:45:58 +0200

    fix du code de retour de help

--- help.c
+++ help.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/08 18:49:52 by snicolet          #+#    #+#             */
-/*   Updated: 2016/05/08 19:02:20 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/05/19 01:44:37 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int		minishell_help(int ac, char **av, t_list **env)
 	(void)env;
 	ft_putstr("Minishell help:\navailables commands: env / cd / unsetenv");
 	ft_putstr(" / purgeenv / export / help / exit\n");
-	return (-1);
+	return (FLAG_BUILTIN);
 }
